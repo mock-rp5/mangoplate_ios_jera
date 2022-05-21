cb12325f7223d3df2f42c571033c9365f4527e71
youngsun <dudtjs1021@hansung.ac.kr>
2022-05-21 14:59:27 +0900

    init: 프로젝트 생성

---
+++ Mangoplate/Mangoplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mangoplate
+//
+//  Created by 임영선 on 2022/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
