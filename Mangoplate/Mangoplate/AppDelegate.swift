c7221a4c1fa02454efcd614755aca573ea64a72d
youngsun <dudtjs1021@hansung.ac.kr>
2022-05-22 12:48:07 +0900

    feat: 카카오 로그인 구현

--- Mangoplate/Mangoplate/AppDelegate.swift
+++ Mangoplate/Mangoplate/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    KakaoSDK.initSDK(appKey: "9e0f03f667c2a87d4cabc3baed37769e")
     return true
   }
 
