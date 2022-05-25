c2344d9d5b49c5dcfa3029064d56f01592a463b6
youngsun <dudtjs1021@hansung.ac.kr>
2022-05-25 19:56:56 +0900

    feat: 망고픽 - 스토리 UI

---
+++ Mangoplate/Mangoplate/Source/Network/MangoPick/Cell/StoryCell.swift
@@ -0,0 +1,20 @@
+//
+//  StoryCell.swift
+//  Mangoplate
+//
+//  Created by 임영선 on 2022/05/25.
+//
+
+import UIKit
+
+class StoryCell: UICollectionViewCell {
+
+  @IBOutlet weak var holicPickImageView: UIImageView!
+  @IBOutlet weak var subTitleLabel: UILabel!
+  @IBOutlet weak var mainTitleLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+
+}
--- Mangoplate/Mangoplate/Source/Network/MangoPick/Controller/StoryViewController.swift
+++ Mangoplate/Mangoplate/Source/Network/MangoPick/Controller/StoryViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class StoryViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  @IBOutlet weak var storyCollectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    storyCollectionView.register(UINib(nibName: "StoryCell", bundle: .main), forCellWithReuseIdentifier: "StoryCell")
+    storyCollectionView.dataSource = self
+    storyCollectionView.delegate = self
+  }
+}
 
-        // Do any additional setup after loading the view.
-    }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension StoryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = storyCollectionView.dequeueReusableCell(withReuseIdentifier: "StoryCell", for: indexPath)
+            as? StoryCell else { return UICollectionViewCell() }
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 3
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let width = storyCollectionView.frame.size.width/2 - 4.5
+    return CGSize(width: width, height: width)
+  }
+  
 }
